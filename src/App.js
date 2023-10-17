e428af0d2eff5f86a93cef9a179a76b6ec4f4abc
Jay Levy <judah.j.levy@gmail.com>
2023-10-17 17:00:55 -0400

    Split Into Preliminary Tabs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+import FoodEntry from './pages/foodEntry.js';
+
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 function App() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Homepage" {...a11yProps(0)} />
+          <Tab label="PersonalData" {...a11yProps(1)} />
+          <Tab label="FoodEntry" {...a11yProps(2)} />
+          <Tab label="WorkoutEntry" {...a11yProps(3)} />
+          <Tab label="SleepEntry" {...a11yProps(4)} />
+          <Tab label="VisualSummary" {...a11yProps(5)} />
+          <Tab label="Calendar" {...a11yProps(6)} />
+        </Tabs>
+      </Box>
+
+      <CustomTabPanel value={value} index={0}>
+        Item One
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        Item Two
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        Item Three
+        <FoodEntry/>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={3}>
+        Item Four
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={4}>
+        Item Five
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={5}>
+        Item Six
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={6}>
+        Item Seven
+      </CustomTabPanel>
+
     </div>
   );
 }
---
+++ src/pages/foodEntry.js
@@ -0,0 +1,7 @@
+function FoodEntry() {
+    return <p>
+        This is the food entry page!
+    </p>;
+}
+
+export default FoodEntry;
