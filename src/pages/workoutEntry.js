dff91ef90684cd678662e791b8f0f907346eac11
Jay Levy <judah.j.levy@gmail.com>
2023-11-09 14:24:59 -0500

    Fix Merge Error

--- src/pages/workoutEntry.js
+++ src/pages/workoutEntry.js
@@ -51,21 +51,21 @@ function WorkoutEntry({ open, setOpen, date, setRefresh }) {
     );
   }
 
-function InnerGrid({ children }) {
-  return (
-    <Grid
-      item
-      container
-      sm={12}
-      spacing={1}
-      direction="column"
-      justifyContent="flex-start"
-      alignItems="stretch"
-    >
-      {children}
-    </Grid>
-  );
-}
+  function InnerGrid({ children }) {
+    return (
+      <Grid
+        item
+        container
+        sm={12}
+        spacing={1}
+        direction="column"
+        justifyContent="flex-start"
+        alignItems="stretch"
+      >
+        {children}
+      </Grid>
+    );
+  }
 
   function OuterGrid({ children }) {
     return (
@@ -97,27 +97,34 @@ function InnerGrid({ children }) {
     );
   }
 
- function storeWorkout(durationHours, durationMinutes, exerciseType, intensity, notes, datetime) {
-  const now = dayjs();
-  const scheduled = dayjs(datetime);
-  const diff = now.diff(scheduled, "minute");
+  function storeWorkout(
+    durationHours,
+    durationMinutes,
+    exerciseType,
+    intensity,
+    notes,
+    datetime
+  ) {
+    const now = dayjs();
+    const scheduled = dayjs(datetime);
+    const diff = now.diff(scheduled, "minute");
 
     let workouts = localStorage.getItem("workouts");
 
-  if (workouts == null || workouts == "") {
-    localStorage.setItem("meals", JSON.stringify([]));
-  }
-  workouts = JSON.parse(localStorage.getItem("workouts"));
+    if (workouts == null || workouts == "") {
+      localStorage.setItem("meals", JSON.stringify([]));
+    }
+    workouts = JSON.parse(localStorage.getItem("workouts"));
 
-  const newWorkout = {
-    id: workouts.length,
-    durationHours: durationHours,
-    durationMinutes: durationMinutes,
-    exerciseType: exerciseType,
-    intensity: intensity,
-    notes: notes,
-    datetime: datetime,
-  };
+    const newWorkout = {
+      id: workouts.length,
+      durationHours: durationHours,
+      durationMinutes: durationMinutes,
+      exerciseType: exerciseType,
+      intensity: intensity,
+      notes: notes,
+      datetime: datetime,
+    };
 
     localStorage.setItem(
       "workouts",
@@ -125,78 +132,78 @@ function InnerGrid({ children }) {
     );
   }
 
- const [durationHours, setDurationHours] = useState('');
- const [durationMinutes, setDurationMinutes] = useState('');
- const [exerciseType, setExerciseType] = useState("");
- const [intensity, setIntensity] = useState("");
- const [notes, setNotes] = useState("");
- const [datetime, setDatetime] = React.useState(date);
+  const [durationHours, setDurationHours] = useState("");
+  const [durationMinutes, setDurationMinutes] = useState("");
+  const [exerciseType, setExerciseType] = useState("");
+  const [intensity, setIntensity] = useState("");
+  const [notes, setNotes] = useState("");
+  const [datetime, setDatetime] = React.useState(date);
 
- return (
-  <Dialog open={open}>
-    <DialogTitle> Add Workout </DialogTitle>
-    <DialogContent>
-      <Container maxWidth="xs">
-        <OuterGrid>
-          <GridItem>
-            <ControlledTextField
-            value={durationHours}
-            setValue={setDurationHours}
-            label="Duration (Hours)"
-        />
-          </GridItem>
-
-          <InnerGrid>
+  return (
+    <Dialog open={open}>
+      <DialogTitle> Add Workout </DialogTitle>
+      <DialogContent>
+        <Container maxWidth="xs">
+          <OuterGrid>
             <GridItem>
               <ControlledTextField
-                value={durationMinutes}
-                setValue={setDurationMinutes}
-                label="Duration (Minutes)"
+                value={durationHours}
+                setValue={setDurationHours}
+                label="Duration (Hours)"
+              />
+            </GridItem>
+
+            <InnerGrid>
+              <GridItem>
+                <ControlledTextField
+                  value={durationMinutes}
+                  setValue={setDurationMinutes}
+                  label="Duration (Minutes)"
+                />
+              </GridItem>
+
+              <GridItem>
+                <FormControl fullWidth>
+                  <InputLabel id="exercise-type-label">
+                    Exercise Type
+                  </InputLabel>
+                  <Select
+                    labelId="exercise-type-label"
+                    id="exercise-type"
+                    value={exerciseType}
+                    label="Exercise Type"
+                    onChange={(e) => setExerciseType(e.target.value)}
+                  >
+                    {exerciseTypes.map((type) => (
+                      <MenuItem key={type} value={type}>
+                        {type}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </GridItem>
+            </InnerGrid>
+
+            <GridItem>
+              <ControlledTextField
+                value={intensity}
+                setValue={setIntensity}
+                label="Intensity (1-10)"
               />
             </GridItem>
 
             <GridItem>
-            <FormControl fullWidth>
-           <InputLabel id="exercise-type-label">Exercise Type</InputLabel>
-           <Select
-             labelId="exercise-type-label"
-             id="exercise-type"
-             value={exerciseType}
-             label="Exercise Type"
-             onChange={(e) => setExerciseType(e.target.value)}
-           >
-             {exerciseTypes.map((type) => (
-               <MenuItem key={type} value={type}>
-                 {type}
-               </MenuItem>
-             ))}
-           </Select>
-         </FormControl>
+              <TextField
+                fullWidth
+                label="Notes"
+                value={notes}
+                setValue={setNotes}
+                multiline
+                rows={4}
+                placeholder={"Write down a small reflection of your workout"}
+                onChange={(e) => setNotes(e.target.value)}
+              />
             </GridItem>
-          </InnerGrid>
-
-      
-          <GridItem>
-            <ControlledTextField
-             value={intensity}
-             setValue={setIntensity}
-             label="Intensity (1-10)"
-            />
-          </GridItem>
-     
-          
-          <GridItem>
-          <TextField
-           fullWidth
-           label="Notes"
-           value={notes}
-           setValue={setNotes}
-           multiline
-           rows={4}
-           placeholder={"Write down a small reflection of your workout"}
-           onChange={(e) => setNotes(e.target.value)}
-         />
-          </GridItem>
 
             <GridItem>
               <DateTimePicker
@@ -214,27 +221,35 @@ function InnerGrid({ children }) {
         </Container>
       </DialogContent>
 
-    <DialogActions>
-      <Button
-        variant="contained"
-        endIcon={<CancelIcon />}
-        onClick={() => setOpen(false)}
-      >
-        Cancel
-      </Button>
-      <Button
-        variant="contained"
-        endIcon={<SendIcon />}
-        onClick={() => {
-          storeWorkout(durationHours, durationMinutes, exerciseType, intensity, notes, datetime);
-          setOpen(false);
-        }}
-      >
-        Add Workout
-      </Button>
-    </DialogActions>
-  </Dialog>
-);
+      <DialogActions>
+        <Button
+          variant="contained"
+          endIcon={<CancelIcon />}
+          onClick={() => setOpen(false)}
+        >
+          Cancel
+        </Button>
+        <Button
+          variant="contained"
+          endIcon={<SendIcon />}
+          onClick={() => {
+            storeWorkout(
+              durationHours,
+              durationMinutes,
+              exerciseType,
+              intensity,
+              notes,
+              datetime
+            );
+            setOpen(false);
+            setRefresh(1);
+          }}
+        >
+          Add Workout
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 }
 
 export default WorkoutEntry;
