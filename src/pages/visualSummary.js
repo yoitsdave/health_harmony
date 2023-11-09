09c2fc9cc0ad39e631b6f064351221c10664211b
Murtaza <murtazaislam1@gmail.com>
2023-11-09 15:36:25 -0500

    vissum

--- src/pages/visualSummary.js
+++ src/pages/visualSummary.js
@@ -1,3 +1,4 @@
+// ... (your existing imports)
 import {
   Container,
   Grid,
@@ -11,16 +12,15 @@ import {
   MenuItem,
 } from "@mui/material";
 
-import { Add as AddIcon } from "@mui/icons-material";
+import { Add as AddIcon, Cancel as CancelIcon } from "@mui/icons-material";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+
 import { BarChart } from '@mui/x-charts/BarChart';
 
-import { Cancel as CancelIcon } from "@mui/icons-material"; // Import CancelIcon
 import dayjs from "dayjs";
 import * as React from "react";
 import Toolbar from "@mui/material/Toolbar";
-
 
 function GridItem({ children }) {
   return (
@@ -44,15 +44,27 @@ function InnerGrid({ children }) {
     </Grid>
   )}
 
-function VisualSummary({setHeader}) {
-  React.useEffect(() => setHeader("Summary Plots"));
-  const [range, setRange] = React.useState("Monthly");
-  const [category, setCategory] = React.useState("Calories Consumed");
-  const [isPopupOpen, setPopupOpen] = React.useState(false);
+function VisualSummary({ setHeader }) {
+  var value = localStorage.getItem("meals");
+  var proper = value ? JSON.parse(value) : null; // Check if value is null
 
-  const handleRangeChange = (event) => {
-    setRange(event.target.value);
-  };
+  // Check if 'proper' is not null and has the expected structure
+  var carbs = proper && proper[0] ? proper[0]["carbs"] : null;
+  var calories = proper && proper[0] ? proper[0]["calories"] : null;
+  var fat = proper && proper[0] ? proper[0]["fat"] : null;
+  var protein = proper && proper[0] ? proper[0]["protein"] : null;
+
+  var workout = JSON.parse(localStorage.getItem("workouts"));
+  var sleep = JSON.parse(localStorage.getItem("sleeps"));
+  var workoutintensity = workout[0] ? workout[0]["intensity"] : null;
+  var workoutduration = workout[0] ? workout[0]["durationHours"] : null;
+
+  var sleepquality = sleep[0] ? sleep[0]["quality"] : null;
+
+  console.log("valuess" + workout)
+  React.useEffect(() => setHeader("Summary Plots"));
+  const [category, setCategory] = React.useState("Macro Stats of most recent meal");
+  const [isPopupOpen, setPopupOpen] = React.useState(false);
 
   const handlePopupOpen = () => {
     setPopupOpen(true);
@@ -68,23 +80,9 @@ function VisualSummary({setHeader}) {
 
   return (
     <Container maxWidth="xs">
-      <Toolbar/>
+      <Toolbar />
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <InnerGrid>
-          <GridItem>
-            <Typography variant="h6">Time Range</Typography>
-            <Select
-              fullWidth
-              label="Time Range"
-              value={range}
-              onChange={handleRangeChange}
-            >
-              <MenuItem value="Monthly">Monthly</MenuItem>
-              <MenuItem value="Weekly">Weekly</MenuItem>
-              <MenuItem value="Daily">Daily</MenuItem>
-            </Select>
-          </GridItem>
-
           <GridItem>
             <Typography variant="h6">Category</Typography>
             <Select
@@ -93,9 +91,11 @@ function VisualSummary({setHeader}) {
               value={category}
               onChange={(event) => setCategory(event.target.value)}
             >
-              <MenuItem value="Calories Consumed">Calories Consumed</MenuItem>
-              <MenuItem value="Time spent sleeping">Time spent sleeping</MenuItem>
-              <MenuItem value="Time spent working out">Time spent working out</MenuItem>
+              <MenuItem value="Macro Stats of most recent meal">Macro Stats of most recent meal</MenuItem>
+              <MenuItem value="Most recent Sleep Quality vs Workout Intensity">Most recent Sleep Quality vs Workout Intensity</MenuItem>
+              <MenuItem value="Most recent Workout Duration vs Workout Intensity">Most recent Workout Duration vs Workout Intensity</MenuItem>
+              <MenuItem value="Most recent Calories intake vs Sleep Quality">Most recent Calories intake vs Sleep Quality</MenuItem>
+
             </Select>
           </GridItem>
 
@@ -115,23 +115,79 @@ function VisualSummary({setHeader}) {
       <Dialog open={isPopupOpen} onClose={handlePopupClose}>
         <DialogTitle>Selected Options</DialogTitle>
         <DialogContent>
-          <BarChart
-            xAxis={[
-              {
-                id: 'barCategories',
-                data: ['bar A', 'bar B', 'bar C'],
-                scaleType: 'band',
-              },
-            ]}
-            series={[
-              {
-                data: [2, 5, 3],
-              },
-            ]}
-            width={500}
-            height={300}
-          />
-          <p>Time : {range}</p>
+          {category === "Macro Stats of most recent meal" && (
+            <BarChart
+              xAxis={[
+                {
+                  id: 'barCategories',
+                  data: ['Calories', 'Protien', 'Carbs','Fat'],
+                  scaleType: 'band',
+                },
+              ]}
+              series={[
+                {
+                  data: [calories,protein, carbs,fat],
+                },
+              ]}
+              width={500}
+              height={300}
+            />
+          )}
+          {category === "Most recent Sleep Quality vs Workout Intensity" && (
+            <BarChart
+              xAxis={[
+                {
+                  id: 'barCategories',
+                  data: ['Sleep Quality', 'Work out Intensity'],
+                  scaleType: 'band',
+                },
+              ]}
+              series={[
+                {
+                  data: [sleepquality, workoutintensity],
+                },
+              ]}
+              width={500}
+              height={300}
+            />
+          )}
+
+          {category === "Most recent Workout Duration vs Workout Intensity" && (
+            <BarChart
+              xAxis={[
+                {
+                  id: 'barCategories',
+                  data: ['Workout Duration(Hr)', 'Workout Intensity'],
+                  scaleType: 'band',
+                },
+              ]}
+              series={[
+                {
+                  data: [workoutduration, workoutintensity],
+                },
+              ]}
+              width={500}
+              height={300}
+            />
+          )}
+                    {category === "Most recent Calories intake vs Sleep Quality" && (
+            <BarChart
+              xAxis={[
+                {
+                  id: 'barCategories',
+                  data: ['Calories Intake', 'Sleep Quality'],
+                  scaleType: 'band',
+                },
+              ]}
+              series={[
+                {
+                  data: [calories, sleepquality],
+                },
+              ]}
+              width={500}
+              height={300}
+            />
+          )}
           <p>Category: {category}</p>
 
           {/* Use a "Cancel" button with the same style as the "SleepEntry" component */}
@@ -139,7 +195,7 @@ function VisualSummary({setHeader}) {
             variant="contained"
             color="primary"
             onClick={handleCancel}
-            endIcon={<CancelIcon />} // Use CancelIcon
+            endIcon={<CancelIcon />}
             style={{ marginLeft: 400 }} 
           >
             Close
